094a4e83d1814206dcc54ffec29122f1c84da41a
Илья <ilak8390@gmail.com>
2022-09-20 21:02:53 +0500

    corrected2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function getRandomNumber(min, max){
   if (min <= 0 || min >= max){
-    throw 'Invalid arguments'
+    throw 'Invalid arguments';
   }
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -9,9 +9,9 @@ getRandomNumber(2, 11);
 
 function checkStringLength(testToString, maxLength){
   if (testToString.length < maxLength){
-     return true;
+    return true;
   }
   return false;
 }
- 
+
 checkStringLength('qwerty', 45);
